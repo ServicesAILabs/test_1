b05c65b81371bd48ab0723216bb11a967a8add39
Lost <lost.babble214@passinbox.com>
2025-06-27 15:34:09 +0200

    -Added Test and Testcase clean_name
    test all funktion, passed test unit, flake8, pytest

--- test/str_test.py
+++ test/str_test.py
@@ -7,7 +7,6 @@ import unittest
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
 )
-print(sys.path)
 
 from string_utils import clean_number
 
@@ -32,6 +31,20 @@ class TestCleanNumber(unittest.TestCase):
             clean_number(None)
 
 
+from string_utils import clean_name
+
+
+class TestCleanName(unittest.TestCase):
+    def test_clean_whitespace(self):
+        self.assertEqual(clean_name("  John  "), "John")
+
+    def test_title(self):
+        self.assertEqual(clean_name("john"), "John")
+
+    def test_is_str(self):
+        with self.assertRaises(TypeError):
+            clean_name(123)
+
+
 if __name__ == "__main__":
     unittest.main()
-    
